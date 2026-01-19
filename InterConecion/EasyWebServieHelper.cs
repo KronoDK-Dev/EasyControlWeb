c105574dc576299ddc2c386cf1e74f284b111941
vybanez <vybanez@simaperu.com>
2026-01-19 16:35:55 -0500

    actualizaciones

--- InterConecion/EasyWebServieHelper.cs
+++ InterConecion/EasyWebServieHelper.cs
@@ -1,15 +1,16 @@
-﻿using System.IO;
-using System.Web.Services.Description;
-using Microsoft.CSharp;
-using System.CodeDom.Compiler;
-using System.CodeDom;
-using System.Net;
-using System.Text;
-using System;
+﻿using EasyControlWeb.Filtro;
 using EasyControlWeb.InterConeccion;
-using EasyControlWeb.Filtro;
-using static EasyControlWeb.EasyUtilitario.Enumerados;
+using Microsoft.CSharp;
+using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Web.Services.Description;
+using static EasyControlWeb.EasyUtilitario.Enumerados;
 
 namespace EasyControlWeb.InterConecion
 {
@@ -28,7 +29,18 @@ namespace EasyControlWeb.InterConecion
 
         public static object InvokeWebService(EasyDataInterConect oEasyDataInterConect)
         {
-            return InvokeWebService("", oEasyDataInterConect);
+            try { 
+              return InvokeWebService("", oEasyDataInterConect);
+            }
+            catch (TargetInvocationException ex)
+            {
+                throw new Exception($"Error al invocar el método 'InvokeWebService': {ex.InnerException?.Message ?? ex.Message}", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error inesperado al invocar el método 'InvokeWebService': {ex.Message}", ex);
+            }
+
         }
         public static object InvokeWebService(string UrlApp, EasyDataInterConect oEasyDataInterConect)
         {
@@ -53,10 +65,20 @@ namespace EasyControlWeb.InterConecion
                 i++;
             }
             string PathApp = UrlApp + oEasyDataInterConect.UrlWebService;
-            return EasyWebServieHelper.InvokeWebService(PathApp, "", oEasyDataInterConect.Metodo, param);
-
+            try { 
+             return EasyWebServieHelper.InvokeWebService(PathApp, "", oEasyDataInterConect.Metodo, param);
+            }
+            catch (TargetInvocationException ex)
+            {
+                throw new Exception($"Error al invocar el método 'InvokeWebService(2)': {ex.InnerException?.Message ?? ex.Message}", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error inesperado al invocar el método 'InvokeWebService(2)': {ex.Message}", ex);
+            }
         }
 
+        
         public static object InvokeWebService(string url, string classname, string methodname, object[] args)
         {
             string @namespace = "ServiceBase.WebService.DynamicWebLoad";
@@ -103,8 +125,136 @@ namespace EasyControlWeb.InterConecion
             object obj = Activator.CreateInstance(t);
             methodname = methodname.Replace("\r\n", "");
             System.Reflection.MethodInfo mi = t.GetMethod(methodname);
-            return mi.Invoke(obj, args);
+            
+            try
+            {
+
+                return mi.Invoke(obj, args);
+            }
+            catch (TargetInvocationException ex)
+            {
+                throw new Exception($"Error al invocar el método '{methodname}': {ex.InnerException?.Message ?? ex.Message}", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error inesperado al invocar el método '{methodname}': {ex.Message}", ex);
+            }
         }
+        // metodo con control de tiempo  12.01.2025
+        public static object InvokeWebService2(string UrlApp, EasyDataInterConect oEasyDataInterConect)
+        {
+            object[] param = new object[oEasyDataInterConect.UrlWebServicieParams.Count]; int i = 0;
+
+            foreach (EasyFiltroParamURLws objParam in oEasyDataInterConect.UrlWebServicieParams)
+            {
+
+                //switch ((TiposdeDatos)System.Enum.Parse(typeof(TiposdeDatos), oEntity["TipoDato"].ToString()))
+                switch (objParam.TipodeDato)
+                {
+                    case TiposdeDatos.String:
+                        param[i] = objParam.Paramvalue;
+                        break;
+                    case TiposdeDatos.Int:
+                        param[i] = Convert.ToInt32(objParam.Paramvalue);
+                        break;
+                    case TiposdeDatos.Double:
+                        param[i] = Convert.ToDouble(objParam.Paramvalue);
+                        break;
+                }
+                i++;
+            }
+            string PathApp = UrlApp + oEasyDataInterConect.UrlWebService;
+            try { 
+            return EasyWebServieHelper.InvokeWebService2(PathApp, "", oEasyDataInterConect.Metodo, param);
+            }
+            catch (TargetInvocationException ex)
+            {
+                throw new Exception($"Error al invocar el método 'InvokeWebService2': {ex.InnerException?.Message ?? ex.Message}", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error inesperado al invocar el método 'InvokeWebService2': {ex.Message}", ex);
+            }
+        }
+        public static object InvokeWebService2(string url, string classname, string methodname, object[] args)
+        {
+            string @namespace = "ServiceBase.WebService.DynamicWebLoad";
+
+            if (string.IsNullOrEmpty(classname))
+            {
+                classname = EasyWebServieHelper.GetClassName(url);
+            }
+
+            // Leer el WSDL con timeout extendido usando la nueva clase WebClient_Tiempo (esta en la carpeta interconecion)
+            using (var wc = new WebClient_Tiempo())
+            {
+                wc.Timeout = 600000; // 10 minutos
+                using (Stream stream = wc.OpenRead(url + "?WSDL"))
+                {
+                    ServiceDescription sd = ServiceDescription.Read(stream);
+                    ServiceDescriptionImporter sdi = new ServiceDescriptionImporter();
+                    sdi.AddServiceDescription(sd, "", "");
+
+                    CodeNamespace cn = new CodeNamespace(@namespace);
+                    CodeCompileUnit ccu = new CodeCompileUnit();
+                    ccu.Namespaces.Add(cn);
+                    sdi.Import(cn, ccu);
+
+                    CSharpCodeProvider csc = new CSharpCodeProvider();
+#pragma warning disable CS0618 // ICodeCompiler es obsoleto pero se requiere en este enfoque
+                    ICodeCompiler icc = csc.CreateCompiler();
+#pragma warning restore CS0618
+
+                    CompilerParameters cplist = new CompilerParameters
+                    {
+                        GenerateExecutable = false,
+                        GenerateInMemory = true
+                    };
+                    cplist.ReferencedAssemblies.Add("System.dll");
+                    cplist.ReferencedAssemblies.Add("System.XML.dll");
+                    cplist.ReferencedAssemblies.Add("System.Web.Services.dll");
+                    cplist.ReferencedAssemblies.Add("System.Data.dll");
+
+                    CompilerResults cr = icc.CompileAssemblyFromDom(cplist, ccu);
+                    if (cr.Errors.HasErrors)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        foreach (CompilerError ce in cr.Errors)
+                        {
+                            sb.AppendLine(ce.ToString());
+                        }
+                        throw new Exception(sb.ToString());
+                    }
+
+                    Assembly assembly = cr.CompiledAssembly;
+                    Type t = assembly.GetType(@namespace + "." + classname, true, true);
+                    object obj = Activator.CreateInstance(t);
+
+                    // Intentar establecer la propiedad Timeout si existe
+                    PropertyInfo timeoutProperty = t.GetProperty("Timeout");
+                    if (timeoutProperty != null && timeoutProperty.CanWrite)
+                    {
+                        timeoutProperty.SetValue(obj, 600000, null); // 10 minutos
+                    }
+
+                    MethodInfo mi = t.GetMethod(methodname);
+                    try
+                    {
+
+                        return mi.Invoke(obj, args);
+                    }
+                    catch (TargetInvocationException ex)
+                    {
+                        throw new Exception($"Error al invocar el método '{methodname}': {ex.InnerException?.Message ?? ex.Message}", ex);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception($"Error inesperado al invocar el método '{methodname}': {ex.Message}", ex);
+                    }
+                }
+            }
+        }
+
         private static string GetClassName(string url)
         {
             string[] parts = url.Split('/');
